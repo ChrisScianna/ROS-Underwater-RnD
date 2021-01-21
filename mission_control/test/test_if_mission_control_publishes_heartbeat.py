05fb7a2ffed0dd015f74102b833287c139777dfa
Alvaro Schapiro <aschapiro@ekumenlabs.com>
2021-01-21 15:24:20 -0300

    Add rostest - jaus ros bridge and Heartbeat

---
+++ mission_control/test/test_if_mission_control_publishes_heartbeat.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+'''
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2020, QinetiQ, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of QinetiQ nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+'''
+import sys
+import os
+import unittest
+import math
+import rosnode
+import rospy
+import rostest
+from mission_control.msg import ReportHeartbeat
+
+
+class TestMissionControlPublishesHeartbeat(unittest.TestCase):
+    """
+        Simulate if mission control publishes heartbeatsion folder.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        rospy.init_node('heartbead_mission_control')
+
+    def setUp(self):
+        self.mission_manager_heart_beat_count = 0
+
+    def callback_mission_manager_heart_beat(self, msg):
+        self.mission_manager_heart_beat_count = msg.seq_id
+
+    def test_mission_control_published_heartbeat(self):
+        rospy.Subscriber('/mission_control/report_heartbeat',
+                         ReportHeartbeat, self.callback_mission_manager_heart_beat)
+
+        while (not rospy.is_shutdown()
+               and self.mission_manager_heart_beat_count < 3):
+            rospy.sleep(0.1)
+
+        self.assertTrue(self.mission_manager_heart_beat_count >= 3)
+
+
+if __name__ == "__main__":
+    rostest.rosrun('mission_control', 'mission_control_heartbeat',
+                   TestMissionControlPublishesHeartbeat)
---
+++ mission_control/test/test_jaus_ros_bridge_interface_mission_control.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+'''
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2020, QinetiQ, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of QinetiQ nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+'''
+import sys
+import os
+import unittest
+import math
+import rosnode
+import rospy
+import rostest
+from mission_control.msg import LoadMission
+from mission_control.msg import ExecuteMission
+from mission_control.msg import ReportExecuteMissionState
+from mission_control.msg import ReportLoadMissionState
+from mission_control.msg import AbortMission
+from mission_control.msg import QueryMissions
+from mission_control.msg import RemoveMissions
+from mission_control.msg import ReportMissions
+
+
+class TestJausRosBridgeInterface(unittest.TestCase):
+    """ 
+        Simulate messages sent by the jaus ros bridge
+    """
+
+    rospy.init_node('thruster_response_if_system_fails')
+    mission_execute_state = ReportExecuteMissionState.PAUSED
+    mission_load_state = False
+    dir_path = os.path.dirname(os.path.abspath(__file__)) + '/test_files/'
+
+    mission_load_state_flag = False
+    report_mission_flag = False
+    report_mission = ReportMissions()
+
+    def callback_mission_execute_state(self, msg):
+        self.report_mission = msg
+        print(msg)
+
+    def callback_report_mission(self, msg):
+        self.report_mission = msg
+        self.report_mission_flag = True
+
+    def callback_mission_load_state(self, msg):
+        self.mission_load_state = msg.load_state
+        self.mission_load_state_flag = True
+
+    def test_jaus_ros_bridge_interface(self):
+        rospy.Subscriber('/mission_control_node/report_mission_execute_state',
+                         ReportExecuteMissionState, self.callback_mission_execute_state)
+
+        rospy.Subscriber('/mission_control_node/report_mission_load_state',
+                         ReportLoadMissionState, self.callback_mission_load_state)
+
+        rospy.Subscriber('/mission_control_node/report_missions',
+                         ReportMissions, self.callback_report_mission)
+
+        simulated_mission_control_load_mission_pub = rospy.Publisher('/mission_control_node/load_mission',
+                                                                     LoadMission, latch=True, queue_size=1)
+
+        simulated_mission_control_execute_mission_pub = rospy.Publisher(
+            '/mission_control_node/execute_mission', ExecuteMission, latch=True, queue_size=1)
+
+        simulated_abort_mission_msg_pub = rospy.Publisher(
+            '/mission_control_node/abort_mission', AbortMission, latch=True, queue_size=1)
+
+        simulated_query_mission_msg_pub = rospy.Publisher(
+            '/mission_control_node/query_missions', QueryMissions, latch=True, queue_size=1)
+
+        simulated_remove_mission_msg_pub = rospy.Publisher(
+            '/mission_control_node/remove_missions', RemoveMissions, latch=True, queue_size=1)
+
+        # TEST - Load a wrong mission
+        mission_to_load = LoadMission()
+        mission_to_load.mission_file_full_path = "NO_MISSION"
+        simulated_mission_control_load_mission_pub.publish(mission_to_load)
+        while (not rospy.is_shutdown() and self.mission_load_state_flag == False):
+            rospy.sleep(0.1)
+        self.assertTrue(self.mission_load_state ==
+                        ReportLoadMissionState.FAILED)
+
+        # TEST - Load a valid Mission
+        self.mission_load_state_flag = False
+        mission_to_load.mission_file_full_path = self.dir_path + "test_missions/mission.xml"
+        simulated_mission_control_load_mission_pub.publish(mission_to_load)
+        while (not rospy.is_shutdown() and self.mission_load_state_flag == False):
+            rospy.sleep(0.1)
+        self.assertTrue(self.mission_load_state ==
+                        ReportLoadMissionState.SUCCESS)
+
+        # TEST - Query Mission
+        mission_to_query = QueryMissions()
+        simulated_query_mission_msg_pub.publish(mission_to_query)
+        while (not rospy.is_shutdown() and self.report_mission_flag == False):
+            rospy.sleep(0.1)
+        self.assertTrue(
+            self.report_mission.missions[0].mission_description == "mission test")
+
+
+if __name__ == "__main__":
+    rostest.rosrun('mission_control', 'mission_control_interface_jause_ros_bridge',
+                   TestJausRosBridgeInterface)
