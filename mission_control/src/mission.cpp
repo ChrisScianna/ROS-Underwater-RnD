ce938360c7764c96377a5a6a3856c75d8a179c49
Alvaro Schapiro <54867318+patapufate@users.noreply.github.com>
2021-03-19 11:45:31 -0300

    Payload command behavior (#107)
    * Add Payload Command behavior
    
    * Add payload command behavior to mission
    
    * change payload behavior tick::running
    
    Co-authored-by: Alvaro Schapiro <aschapiro@ekumenlabs.com>

--- mission_control/src/mission.cpp
+++ mission_control/src/mission.cpp
@@ -42,6 +42,7 @@
 #include "mission_control/behaviors/fixed_rudder.h"
 #include "mission_control/behaviors/depth_heading.h"
 #include "mission_control/behaviors/attitude_servo.h"
+#include "mission_control/behaviors/payload_command.h"
 
 #include <string>
 
@@ -68,6 +69,7 @@ class MissionBehaviorTreeFactory : public BT::BehaviorTreeFactory
     this->registerNodeType<mission_control::MoveWithFixedRudder>("MoveWithFixedRudder");
     this->registerNodeType<mission_control::DepthHeadingBehavior>("DepthHeadingBehavior");
     this->registerNodeType<mission_control::AttitudeServoBehavior>("AttitudeServoBehavior");
+    this->registerNodeType<mission_control::PayloadCommandBehavior>("PayloadCommandBehavior");
   }
 };
 
