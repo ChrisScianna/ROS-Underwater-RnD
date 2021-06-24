05b130c5204cf844ceaa9b96c41541e579530178
Michel Hidalgo <michel@ekumenlabs.com>
2021-06-24 15:27:27 -0300

    Drop mission_logger package

--- mission_logger/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['mission_logger'],
-    package_dir={'': 'src'}
-)
-
-setup(**d)
--- mission_logger/src/mission_logger/common.py
+++
@@ -1,3 +0,0 @@
-
-def textindent(text, offset=4):
-    return ''.join([' ' * offset + line for line in text.splitlines(True)])
